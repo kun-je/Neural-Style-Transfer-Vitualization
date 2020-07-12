5d81c9878f626018c2ec989deebad9bd60bea77b
ED_Garden <kunchayasoinak@gmail.com>
2020-07-12 22:05:20 +0700

    make a file which contains code that can be check using Spyder

---
+++ NST_project/locallibrary/catalog/in_spyder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 21:53:59 2020
+
+@author: kun-je
+NST project in spyder
+"""
+#is project is done using "A Neural Algorithm of Artistic Style
+# by. Leon A. Gatys,  Alexander S. Ecker, Matthias Bethge" as a reference
+
+#this is for read image from a file
+def load_image():
+    return 0;
+
+#this is to save image in the databased
+def save_image():
+    return 0;
+
+#this is for plotting iage using mathplotlib, to show image
+def plot_image():
+    return 0;
+
+
+def loss_function():
+    return 0 ;
