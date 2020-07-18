185042c5e85565d9b7e7e9f7b3dd50c82a74f836
ED_Garden <kunchayasoinak@gmail.com>
2020-07-18 21:53:36 +0700

    make vgg16 function

--- NST_project/locallibrary/catalog/in_spyder.py
+++ NST_project/locallibrary/catalog/in_spyder.py
@@ -5,22 +5,36 @@ Created on Sun Jul 12 21:53:59 2020
 
 @author: kun-je
 NST project in spyder
+
+Thisis project is done using "A Neural Algorithm of Artistic Style
+ by. Leon A. Gatys,  Alexander S. Ecker, Matthias Bethge" as a reference
 """
-#is project is done using "A Neural Algorithm of Artistic Style
-# by. Leon A. Gatys,  Alexander S. Ecker, Matthias Bethge" as a reference
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+from keras.applications.vgg16 import VGG16
+model = VGG16()
 
 #this is for read image from a file
 def load_image():
-    return 0;
+    return 0
 
 #this is to save image in the databased
 def save_image():
-    return 0;
+    return 0
 
 #this is for plotting iage using mathplotlib, to show image
 def plot_image():
-    return 0;
+    return 0
 
+#this loss function use mean square error to find
+#the different between the actual matrix and predicted matrix
+def loss_function(matrix_true, matrix_pred):
+    return tf.keras.losses.MSE(matrix_true,matrix_pred)
 
-def loss_function():
-    return 0 ;
+#gram matrix
+def gram_matrix(tensor):
+    return 0
