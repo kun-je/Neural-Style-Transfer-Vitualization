b6b46348e18b954575b613bc874821dd027f659b
ED_Garden <kunchayasoinak@gmail.com>
2020-07-20 22:03:33 +0700

    make plot image and load image function

--- NST_project/locallibrary/catalog/in_spyder.py
+++ NST_project/locallibrary/catalog/in_spyder.py
@@ -12,23 +12,39 @@ Thisis project is done using "A Neural Algorithm of Artistic Style
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import tensorflow as tf
-
-
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
 from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
 model = VGG16()
 
-#this is for read image from a file
-def load_image():
-    return 0
+#load style image and preprocess it for vgg-16 and return origianl image and
+#already preprocess image
+def load_image(file_name):
+    #load image
+    image = load_img(file_name, target_size=(224, 224))
+    #convert image to pixel in array
+    img = img_to_array(image)
+    img = img.reshape((-1, img.shape[0], img.shape[1], img.shape[2]))
+    #preprocess it for VGG-16,change the image to the tensor
+    img = preprocess_input(img)   
+    return image,img 
 
+
+
+#show context image
+def plot_img(load_image):
+    plt.imshow(load_image)
+    plt.show()
+    
+    
 #this is to save image in the databased
 def save_image():
     return 0
 
-#this is for plotting iage using mathplotlib, to show image
-def plot_image():
-    return 0
 
 #this loss function use mean square error to find
 #the different between the actual matrix and predicted matrix
@@ -38,3 +54,6 @@ def loss_function(matrix_true, matrix_pred):
 #gram matrix
 def gram_matrix(tensor):
     return 0
+
+image = load_image('cat.jpeg')
+plot_img(image[0])
