310aa97ffd0a902b8f4af44873d4bde753521ca8
ED_Garden <kunchayasoinak@gmail.com>
2020-08-09 19:21:56 +0700

    merge content

--- NST_project/locallibrary/catalog/neural1.py
+++ NST_project/locallibrary/catalog/neural1.py
@@ -17,22 +17,6 @@ import tensorflow.keras.preprocessing.image as img
 from tensorflow.keras.applications.vgg16 import decode_predictions
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
-<<<<<<< HEAD
-from keras.optimizers import Adam
-
-
-MODEL = VGG16()
-CONTENT_LAYERS = ['block5_conv2']
-STYLE_LAYERS = ['block1_conv1',
-                'block2_conv1',
-                'block3_conv1',
-                'block4_conv1',
-                'block5_conv1']
-
-
-def load_image(image_path):
-    """
-=======
 
 
 def aspect_ratio(image_path):
@@ -57,30 +41,15 @@ def aspect_ratio(image_path):
 
 def load_image(image_path):
     """
-        Description: 
+        Description:
             As we are using a pre-trained version VGG16 we have to resize and normalise
             the inputs.
->>>>>>> content
         Args:
             image_path (str): This takes a given an image path
         Returns:
             <class 'numpy.ndarray'> : This would convert the given image into array
             <class 'PIL.Image.Image'>: This would convert the given image into PIL format
     """
-<<<<<<< HEAD
-    IMG_SIZE = 224
-    CHANNEL = 3
-    image_array = img.load_img(image_path, target_size=(IMG_SIZE, IMG_SIZE))
-    image = img.img_to_array(image_array)
-    image = image.reshape((1, IMG_SIZE, IMG_SIZE, CHANNEL))
-    image = preprocess_input(image)
-    return image, image_array
-
-
-def plot_img(image_path):
-    """
-        This function shows a plotted graph of the given image
-=======
     image_array = img.load_img(image_path, target_size=(IMG_HEIGHT, IMG_WIDTH))
     image = img.img_to_array(image_array)
     image = image.reshape((1, IMG_HEIGHT, IMG_WIDTH, CHANNEL))
@@ -89,7 +58,7 @@ def plot_img(image_path):
 
 def deprocess_img(image):
     """
-        Description: 
+        Description:
             This is used to reverse the depressing of the image. This is used in order
             to get the image.
         Args:
@@ -112,31 +81,23 @@ def tensor_inputs(c_image_path, g_image_path, s_image_path):
     g_image = load_image(g_image_path)[0]
     s_image = load_image(s_image_path)[0]
     return c_image, g_image, s_image
-    
+
 
 def plot_img(image_path):
     """
-        Description: 
+        Description:
             This function shows a plotted graph of the given image
->>>>>>> content
         Args:
             image_path (str): This would take an image path
     """
     image = load_image(image_path)[1]
     plt.imshow(image)
     plt.show()
-<<<<<<< HEAD
 
 
-def save_image():
-    """TODO"""
-    return 0
-=======
-    
-    
 def save_image(file_name, array_image):
     """
-        Description: 
+        Description:
             This saves a given tensor image and saves the generated file into
             an output folder
         Args:
@@ -146,47 +107,19 @@ def save_image(file_name, array_image):
     file_name = "output/{}".format(file_name)
     img.save_img(file_name, deprocess_img(array_image))
     return True
->>>>>>> content
 
 def MSE(matrix_content, matrix_generated):
     """
         Args:
-<<<<<<< HEAD
             matrix_content (<class 'numpy.ndarray'>):
             matrix_generated (<class 'numpy.ndarray'>):
         Returns:
             int: A number made by perform substraction operation from each matrix (tensor), followed by
-=======
-            matrix_content (<class 'numpy.ndarray'>): 
-            matrix_generated (<class 'numpy.ndarray'>):
-        Returns:
-            int: A number made by perform substraction operation from each matrix (tensor), followed by 
->>>>>>> content
                 squared operation with each substraction operation. The operation reduce mean is then applied.
     """
     return tf.reduce_mean(tf.square(matrix_content - matrix_generated))
 
 
-<<<<<<< HEAD
-def get_layer(image_path, layer_name):
-    """
-        Args:
-            image_path (str): A given image path
-            layer_name (str): A given layer name within the cnn model
-        Returns:
-            <class 'numpy.ndarray'> :
-
-    """
-    image = load_image(image_path)[0]
-    layer = tf.keras.Model(inputs=MODEL.inputs, outputs=MODEL.get_layer(layer_name).output)
-    return layer.predict(image)
-
-def get_weights(layer_name):
-    """
-        Args:
-            layer_name (str): A layer name within the cnn model
-
-=======
 def get_layer(image, layer_name):
     """
         Args:
@@ -197,12 +130,11 @@ def get_layer(image, layer_name):
     """
     layer = tf.keras.Model(inputs=MODEL.inputs, outputs=MODEL.get_layer(layer_name).output)
     return layer.predict(image)
-   
+
 def get_weights(layer_name):
     """
-        Args: 
+        Args:
             layer_name (str): A layer name within the cnn model
->>>>>>> content
         Returns:
             List<int>: A lists of nump array containing weights corresponding to the given layer
     """
@@ -211,16 +143,6 @@ def get_weights(layer_name):
             return layer.get_weights()
 
 
-<<<<<<< HEAD
-def content_loss_function(c_image_path, g_image_path, layer_name):
-    """
-        Args:
-            c_image_path (str): To take the content image path
-            g_image_path (str): To take the generate image path
-
-        Returns:
-            int: The loss content. A low integer denotes the content is similar
-=======
 def content_loss_function(c_image, g_image, layer_name):
     #todo need to change doc string as type was changed
     """
@@ -229,28 +151,11 @@ def content_loss_function(c_image, g_image, layer_name):
             g_image_path (str): To take the generate image path
             layer_name (str): To take in the layer name
         Returns:
-            int: The loss content. A low integer denotes the content is similar 
->>>>>>> content
+            int: The loss content. A low integer denotes the content is similar
             to the generated image. A high integer denotes the content is not similar
             to the generated image
     """
     WEIGHT = 0.5
-<<<<<<< HEAD
-    generated_layer = get_layer(g_image_path, layer_name)
-
-    content_layer = get_layer(c_image_path, layer_name)
-    loss = MSE(generated_layer, content_layer)
-    return WEIGHT*loss
-
-
-def gram_matrix(tensor):
-    """
-        Args:
-            tensor (tensor): take 4D tensor
-
-        Returns:
-            gram (tensor) : gram matrix which is 2D array of the multiplication
-=======
     generated_layer = get_layer(g_image, layer_name)
     content_layer = get_layer(c_image, layer_name)
     loss = MSE(generated_layer, content_layer)
@@ -262,17 +167,16 @@ def gradient_content_loss(c_image, g_image, layer_name):
         tape.watch(g_image)
         content_loss =  content_loss_function(c_image, g_image, layer_name)
     return tape.gradient(content_loss, g_image)
-   
+
 
 
 def gram_matrix(tensor):
     """
-        Args: 
+        Args:
             tensor (tensor): take 3D tensor
-            
+
         Returns:
-            gram (tensor) : gram matrix which is 2D array of the multiplication 
->>>>>>> content
+            gram (tensor) : gram matrix which is 2D array of the multiplication
             of the reshape matrix and its transpose
     """
     m_shape = []
@@ -280,91 +184,40 @@ def gram_matrix(tensor):
     m_shape.append(tensor.shape[1]*tensor.shape[2])
     tensor = tf.reshape(tensor,m_shape)
     gram = tf.matmul(tensor,tf.transpose(tensor))
-<<<<<<< HEAD
-    return gram;
-
-def style_loss_function(s_image_path, g_image_path, layer_name):
-=======
     return gram
 
 def style_loss_function(s_image, g_image, layer_name):
->>>>>>> content
     """
         Args:
             c_image_path (str): To take the style image path
             g_image_path (str): To take the generate image path
-<<<<<<< HEAD
-
         Returns:
             int: The loss content. A low integer denotes the content is similar
             to the generated image. A high integer denotes the content is not similar
             to the generated image
 
     """
-    generated_layer = get_layer(g_image_path, layer_name)
-    style_layer = get_layer(s_image_path, layer_name)
-
-    #finding gram matrix of s and g image from perticular layer
-    generated_gram = gram_matrix(generated_layer)
-    style_gram = gram_matrix(style_layer)
-
-    channel = 3
-    img_size = 224 * 224
-    
-    #each layer of total style loss
-    loss = MSE(generated_gram, style_gram)/(4*(channel**2)*(img_size**2))
-    return loss
-
-def total_variation_loss(g_image):
-    weight = 30
-    loss = weight*tf.reduce_sum(tf.image.total_variation(g_image))
-    return loss
-
-def style_grad_optimizer():
-    x = tf.Variable(2, name = 'x', dtype = tf.float32)
-    
-    return 0
-
-def optimizer(learning_rate, beta1, beta2):
-    adam = tf.keras.optimizers.Adam(learning_rate,beta1,beta2)
-    return adam
-
-
-image_path = 'cat.jpeg'
-image = load_image('cat.jpeg')
-num = content_loss_function("cat.jpeg", "noise.jpg", CONTENT_LAYERS[0])
-print(num)
-
-s = style_loss_function("cat.jpeg", "noise.jpg", STYLE_LAYERS[0])
-print(s)
-=======
-        Returns:
-            int: The loss content. A low integer denotes the content is similar 
-            to the generated image. A high integer denotes the content is not similar
-            to the generated image
-  
-    """
 
     generated_layer = get_layer(g_image, layer_name)
     style_layer = get_layer(s_image, layer_name)
-    
+
     #finding gram matrix of s and g image from perticular layer
     generated_gram = gram_matrix(generated_layer)
     style_gram = gram_matrix(style_layer)
-    
+
     img_size = IMG_HEIGHT * IMG_WIDTH
-    
+
     loss = MSE(generated_gram, style_gram)/(4*(CHANNEL**2)*(img_size**2))
     return loss
-    
+
 if __name__ == "__main__":
     MODEL = VGG16()
-    CONTENT_LAYERS = ['block5_conv2'] 
+    CONTENT_LAYERS = ['block5_conv2']
     STYLE_LAYERS = ['block1_conv1',
                 'block2_conv1',
-                'block3_conv1', 
-                'block4_conv1', 
-                'block5_conv1'] 
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1']
     image_path = "dog.jpg"
     noise_path = "noise.jpg"
     style_path = "style.jpg"
@@ -385,4 +238,3 @@ if __name__ == "__main__":
     print(s)
 
     gradient_content_loss(c_image, c_image, CONTENT_LAYERS[0])
->>>>>>> content
